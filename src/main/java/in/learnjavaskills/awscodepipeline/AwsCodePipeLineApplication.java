2cf221088eaca879b14275aabaf8039f6a693ab5
shaikh-imran1 <imranshaikh5937@gmail.com>
2020-09-05 16:14:59 +0530

    First commit

---
+++ src/main/java/in/learnjavaskills/awscodepipeline/AwsCodePipeLineApplication.java
@@ -0,0 +1,13 @@
+package in.learnjavaskills.awscodepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodePipeLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodePipeLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/learnjavaskills/awscodepipeline/AwsCodePipeLineApplicationTests.java
@@ -0,0 +1,16 @@
+package in.learnjavaskills.awscodepipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsCodePipeLineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
