e963821635f5c2805c3b40b57793e32b3ec1987a
shaikh-imran1 <imranshaikh5937@gmail.com>
2020-09-05 17:05:28 +0530

    second build test

--- src/main/java/in/learnjavaskills/awscodepipeline/controller/PageController.java
+++ src/main/java/in/learnjavaskills/awscodepipeline/controller/PageController.java
@@ -1,5 +1,8 @@
 package in.learnjavaskills.awscodepipeline.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,11 @@ public class PageController
 	public String message(@PathVariable String message)
 	{
 		return message;
+	}
+	
+	@GetMapping("getNames")
+	public List<String> getNames()
+	{
+		return Arrays.asList("Arshad","Please", "serve", "tea","to","imran");
 	}
 }
